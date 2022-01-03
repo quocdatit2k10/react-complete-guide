6dac08f54ba4c2e39d7408a5c7cf5c5ceb1876de
dat <quocdatit2k10@gmail.com>
2022-01-04 10:33:08 +1100

    refactor expenses list

--- src/compomemts/Expenses/Expenses.js
+++ src/compomemts/Expenses/Expenses.js
@@ -1,8 +1,9 @@
 import { useState } from "react/cjs/react.development";
 import Card from "../UI/Card";
-import ExpenseItem from "./ExpenseItem";
+
 import "./Expenses.css";
 import ExpensesFilter from "./ExpensesFilter";
+import ExpensesList from "./ExpensesList";
 
 function Expenses(props) {
   const [filterYear, setFilterYear] = useState("2022");
@@ -15,20 +16,13 @@ function Expenses(props) {
     return expense.date.getFullYear().toString() === filterYear;
   });
 
-  let expensesContent = <p>No expenses found.</p>;
-  if (filteredExpenses.length > 0) {
-    expensesContent = filteredExpenses.map((e) => (
-      <ExpenseItem key={e.id} title={e.title} amount={e.amount} date={e.date} />
-    ));
-  }
-
   return (
     <Card className="expenses">
       <ExpensesFilter
         selected={filterYear}
         onChangeFilter={filterChangeHandler}
       />
-      {expensesContent}
+      <ExpensesList items={filteredExpenses} />
     </Card>
   );
 }
